ec87763ca0b982b7b733b1e453d50c33d4c9ab34
Prachet <prachetm8@gmail.com>
2026-04-26 07:44:32 +0530

    HelioHost database connection

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:8080";
-
+const BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
 
 
 const getToken = () => sessionStorage.getItem("token");
